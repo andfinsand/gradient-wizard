e8cf8af318afec3c1db468e8459ce8e46628eb79
Andrew Finsand <andfinsand@gmail.com>
2023-02-09 10:30:31 -0800

    More styling.

--- frontend/src/components/HexDiv.js
+++ frontend/src/components/HexDiv.js
@@ -4,8 +4,8 @@ const HexDiv = () => {
 
     return (
         <div className="flex justify-center text-black mt-2">
-            <div className="bg-glassLight border-2 border-glassLight rounded-full shadow-custom-hex font-semibold m-5 px-12 py-3">#7F00FF</div>
-            <div className="bg-glassLight border-2 border-glassLight rounded-full shadow-custom-hex font-semibold m-5 px-12 py-3">#E100FF</div>
+            <div className="bg-glassLight border-2 border-glassLight rounded-full shadow-custom-hex font-semibold m-5 px-12 py-3">#003049</div>
+            <div className="bg-glassLight border-2 border-glassLight rounded-full shadow-custom-hex font-semibold m-5 px-12 py-3">#00B2D6</div>
         </div>
     );
 }
