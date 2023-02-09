f8234ead8b14bf119a1110a821bf04c74382f98d
Andrew Finsand <andfinsand@gmail.com>
2023-02-09 10:38:23 -0800

    Tailwindconfig 'glassLightest'

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
       grayDark: '#3F4C6B',
       glassMedium: 'rgba(255, 255, 255, 0.5)',
       glassLight: 'rgba(255, 255, 255, 0.3)',
-      glassLightest: 'rgba(155, 181, 255, 0.3)',
+      glassLightest: 'rgba(255, 255, 255, 0.1)',
       glassDark: 'rgba(51, 51, 51, 0.75)',
     },
     borderRadius: {
