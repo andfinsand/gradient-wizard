00d0b7c088e324890fcabf1e0e9adb638c594712
Andrew Finsand <andfinsand@gmail.com>
2023-02-12 12:14:27 -0800

    Copy hex code on click.

--- frontend/src/components/HexDiv.js
+++ frontend/src/components/HexDiv.js
@@ -2,16 +2,21 @@ import React, { useState } from "react";
 
 const HexDiv = ({data}) => {
 
+    const copyToClipboard = (e) => {
+        const code = e.currentTarget.querySelector(".hex-code").textContent;
+        navigator.clipboard.writeText(code);
+      }
+
     return (
         <div className="flex justify-center text-black mt-2">
-            <div className="flex justify-center bg-glassLight border-2 border-glassLight rounded-full shadow-custom-hex font-semibold w-40 m-5 py-3">
+            <button onClick={copyToClipboard} className="flex justify-center bg-glassLight border-2 border-glassLight rounded-full shadow-custom-hex font-semibold w-40 m-5 py-3">
                 <div style={{ backgroundColor: data.hex1 || "#003049" }} className="self-center w-3 h-3 mr-3"></div>
-                {data.hex1 || "#003049"}
-            </div>
-            <div className="flex justify-center bg-glassLight border-2 border-glassLight rounded-full shadow-custom-hex font-semibold w-40 m-5 py-3">
+                <span className="hex-code">{data.hex1 || "#003049"}</span>
+            </button>
+            <button onClick={copyToClipboard} className="flex justify-center bg-glassLight border-2 border-glassLight rounded-full shadow-custom-hex font-semibold w-40 m-5 py-3">
                 <div style={{ backgroundColor: data.hex2 || "#00B2D6" }} className="self-center w-3 h-3 mr-3"></div>
-                {data.hex2 || "#00B2D6"}
-            </div>
+                <span className="hex-code">{data.hex2 || "#00B2D6"}</span>
+            </button>
         </div>
     );
 }
