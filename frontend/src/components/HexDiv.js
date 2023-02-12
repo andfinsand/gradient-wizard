8055abbaf8800e598751c1ac1d6d5f61d5a1ecdd
Andrew Finsand <andfinsand@gmail.com>
2023-02-12 13:40:00 -0800

    Uninstalled tinycolor.

--- frontend/src/components/HexDiv.js
+++ frontend/src/components/HexDiv.js
@@ -9,11 +9,11 @@ const HexDiv = ({data}) => {
 
     return (
         <div className="flex justify-center text-black mt-2">
-            <button onClick={copyToClipboard} className="flex justify-center bg-glassLight border-2 border-glassLight rounded-full shadow-custom-hex font-semibold w-40 m-5 py-3">
+            <button onClick={copyToClipboard} className="flex justify-center bg-glassLight hover:bg-glassLightest border-2 border-glassLight rounded-full shadow-custom-hex font-semibold w-40 m-5 py-3">
                 <div style={{ backgroundColor: data.hex1 || "#003049" }} className="self-center w-3 h-3 mr-3"></div>
                 <span className="hex-code">{data.hex1 || "#003049"}</span>
             </button>
-            <button onClick={copyToClipboard} className="flex justify-center bg-glassLight border-2 border-glassLight rounded-full shadow-custom-hex font-semibold w-40 m-5 py-3">
+            <button onClick={copyToClipboard} className="flex justify-center bg-glassLight hover:bg-glassLightest border-2 border-glassLight rounded-full shadow-custom-hex font-semibold w-40 m-5 py-3">
                 <div style={{ backgroundColor: data.hex2 || "#00B2D6" }} className="self-center w-3 h-3 mr-3"></div>
                 <span className="hex-code">{data.hex2 || "#00B2D6"}</span>
             </button>
