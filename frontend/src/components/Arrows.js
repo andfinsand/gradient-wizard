3194525e01958b2ac4ea629875ebaaad8c52e477
Andrew Finsand <andfinsand@gmail.com>
2023-02-13 23:13:36 -0800

    Fade in and out effect for previous and forward error message.

--- frontend/src/components/Arrows.js
+++ frontend/src/components/Arrows.js
@@ -5,15 +5,20 @@ import Chevron_Right from "../../static/svg/Chevron_Right_Icon.svg";
 const Arrows = ({handlePrevious, handleNext, currentDataIndex, history}) => {
     const [prevMessage, setPrevMessage] = useState("");
     const [nextMessage, setNextMessage] = useState("");
+    const [prevFadeIn, setPrevFadeIn] = useState(false);
+    const [prevFadeOut, setPrevFadeOut] = useState(false);
+    const [nextFadeIn, setNextFadeIn] = useState(false);
+    const [nextFadeOut, setNextFadeOut] = useState(false);
 
     const handlePrevClick = () => {
         if (currentDataIndex === 0) {
             handlePrevious();
         } else if (currentDataIndex < 1) {
-            setPrevMessage("No saved gradients");
+            setPrevMessage("No saved gradients!");
+            setPrevFadeIn(true);
             setTimeout(() => {
-                setPrevMessage("");
-            }, 2000);
+                setPrevFadeOut(true);
+            }, 2000); // remove message after 3 seconds
         } else {
             handlePrevious();
         }
@@ -22,9 +27,10 @@ const Arrows = ({handlePrevious, handleNext, currentDataIndex, history}) => {
     const handleNextClick = () => {
         if (currentDataIndex === history.length - 1) {
             setNextMessage("Create more gradients!");
+            setNextFadeIn(true);
             setTimeout(() => {
-                setNextMessage("");
-            }, 2000);
+                setNextFadeOut(true);
+            }, 2000); // remove message after 3 seconds
         } else {
             handleNext();
         }
@@ -33,27 +39,47 @@ const Arrows = ({handlePrevious, handleNext, currentDataIndex, history}) => {
     return (
         <div className="flex justify-between w-full text-white mb-5">
             {/* Go back button */}
-            <button onClick={handlePrevClick}>
+            <div className="relative mx-3">
+                <button onClick={handlePrevClick}>
+                    <div className="bg-glassLight px-4 py-8">
+                        <img src={Chevron_Left} alt="Chevron_Left" className="text-white"/>
+                    </div>
+                </button>
                 {prevMessage && (
-                    <div className="flex self-center bg-glassLight text-black text-sm absolute rounded-md w-32 text-center">
-                        {prevMessage}
-                    </div>
-                )}
-                <div className="bg-glassLight px-4 py-8">
-                    <img src={Chevron_Left} alt="Chevron_Left" className="text-white"/>
+                <div
+                    className={`flex justify-center items-center bg-glassLight text-black text-sm italic absolute rounded-md w-34 h-9 top-0 transform -translate-y-16 -translate-x-9 ${prevFadeIn ? "fade-in" : ""} ${prevFadeOut ? "fade-out" : ""}`}
+                    onAnimationEnd={() => {
+                        if (prevFadeOut) {
+                            setPrevMessage("");
+                            setPrevFadeOut(false);
+                        }
+                    }}
+                >
+                    {prevMessage}
                 </div>
-            </button>
+                )}
+            </div>
             {/* Go forward button */}
-            <button onClick={handleNextClick}>
-                {nextMessage && (
-                    <div className="bg-glassLight text-black text-sm absolute rounded-md w-24 text-center">
-                        {nextMessage}
+            <div className="relative mx-3">
+                <button onClick={handleNextClick}>
+                    <div className="bg-glassLight px-4 py-8">
+                        <img src={Chevron_Right} alt="Chevron_Left" className="text-white"/>
                     </div>
-                )}
-                <div className="bg-glassLight px-4 py-8">
-                    <img src={Chevron_Right} alt="Chevron_Left" className="text-white"/>
+                </button>
+                {nextMessage && (
+                <div
+                    className={`flex justify-center items-center bg-glassLight text-black text-sm italic absolute rounded-md w-38 h-9 top-0 right-0 transform -translate-y-16 translate-x-10 ${nextFadeIn ? "fade-in" : ""} ${nextFadeOut ? "fade-out" : ""}`}
+                    onAnimationEnd={() => {
+                        if (nextFadeOut) {
+                            setNextMessage("");
+                            setNextFadeOut(false);
+                        }
+                    }}
+                >
+                    {nextMessage}
                 </div>
-            </button>
+                )}
+            </div>
         </div>
     );
 };
