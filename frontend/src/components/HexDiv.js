012e6cc9b7a299c76e870726630b1405cc9a3633
Andrew Finsand <andfinsand@gmail.com>
2023-03-27 12:45:58 -0700

    Third hex code visible if three gradients selected.

--- frontend/src/components/HexDiv.js
+++ frontend/src/components/HexDiv.js
@@ -49,6 +49,23 @@ const HexDiv = ({currentData}) => {
                     </div>
                 )}
             </button>
+
+            {/* hex 3 */}
+            {currentData.hex3 && (
+                <button onClick={copyToClipboard} data-index={2} className="flex justify-center self-center bg-background-gradient hover:bg-glassMedium rounded-full shadow-custom-hex text-xs font-semibold w-32 m-5 py-4 hover:transition ease-in-out duration-300">
+                    {copiedMessage[2] ? (
+                        <div className="flex items-center">
+                            <span className="flex text-black text-xs italic tracking-wider">{copiedMessage[2]}</span>
+                        </div>
+                    ) : (
+                        <div className="flex items-center">
+                            <div style={{ backgroundColor: currentData.hex3 }} className="self-center w-3 h-3 mr-3"></div>
+                            <span className="hex-code">{currentData.hex3.toUpperCase()}</span>
+                        </div>
+                    )}
+                </button>
+)}
+
         </div>
     );
 }
