acd48bb82f3296813d2b4b5c548fd3125c59fbc8
Andrew Finsand <andfinsand@gmail.com>
2023-04-04 14:14:42 -0700

    Include import os to settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
