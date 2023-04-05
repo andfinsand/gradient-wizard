d6791e04f7f15b17b79df7190e981817a5997873
Andrew Finsand <andfinsand@gmail.com>
2023-04-05 13:16:24 -0700

    Updated allowed host for future domain names.

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'gradient-wizard.up.railway.app',
+    'gradientwizard.io',
+]
 
 
 # Application definition
