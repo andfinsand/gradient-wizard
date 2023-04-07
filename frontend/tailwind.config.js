97e348a1725388f1e77f9df77129e49d4ca58c91
Andrew Finsand <andfinsand@gmail.com>
2023-04-07 14:01:57 -0700

    Input field CSS styling removed from tailwind.config and added to style.css for iOS.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -53,7 +53,6 @@ module.exports = {
         'custom-navbar': '0 2px 3px rgb(0, 0, 0, 0.15), inset 0 -1px 3px rgb(0, 0, 0, 0.15), inset 0 3px 5px rgb(255, 255, 255, 0.2)',
         'custom-main': '0 35px 12px -28px rgb(0, 0, 0, 0.2), inset 0 -1px 3px rgb(0, 0, 0, 0.2), inset 0px 25px 10px -20px rgba(255, 255, 255, 0.1)',
         'custom-hex': '0 17px 8px -12px rgb(0, 0, 0, 0.2), inset 0 -1px 2px rgb(0, 0, 0, 0.2), inset 0px 25px 10px -20px rgba(255, 255, 255, 0.1)',
-        'custom-input-inner': 'inset 2px 4px 7px 0 rgba(0, 0, 0, 0.22), inset -3px -4px 7px 0 rgba(255, 255, 255, 0.5)',
         'custom-button': '3px 3px 5px rgba(0, 0, 0, 0.15), inset -2px -2px 3px rgba(0, 0, 0, 0.2), inset 2px 2px 3px rgba(255, 255, 255, 0.25)',
         'custom-button-press': '0 2px 2px rgba(0, 0, 0, 0.1), inset 2px 2px 8px rgba(0, 0, 0, 0.2), inset -2px -2px 8px rgba(255, 255, 255, 0.2)',
         'custom-button-style-slide': '2px 2px 4px rgba(0, 0, 0, 0.2), inset -2px -2px 5px rgba(0, 0, 0, 0.15), inset 2px 2px 5px rgba(255, 255, 255, 0.2)',
